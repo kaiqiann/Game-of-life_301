d69e5d73da085708d8f872245b16b971aa1e6480
zjc1004 <35477606+zjc1004@users.noreply.github.com>
2019-12-04 18:51:00 -0500

    Add files via upload
    Modify Selector

--- src/main/java/edu/neu/coe/info6205/life/base/Selector.java
+++ src/main/java/edu/neu/coe/info6205/life/base/Selector.java
@@ -9,8 +9,8 @@ class Selector {
     public static void Select(List<String> patternList) {
         Map<String, Long> select = new HashMap<>();
         for(String pattern: patternList){
-            Behavior generation = Game.run(0L, pattern);
-            select.put(pattern, generation.generation);
+            long generation = Fitness(pattern);
+            select.put(pattern, generation);
         }
 
         List<Map.Entry<String, Long>> list = new ArrayList<>(select.entrySet());
@@ -25,5 +25,28 @@ class Selector {
         for (int i=0; i<list.size()/2; i++) {
             patternList.add(list.get(i).getKey());
         }
+    }
+
+    public static long Fitness(String pattern){
+        Behavior generations = Game.run(0L, pattern);
+        return generations.generation; 
+    }
+
+    public static String getBest(List<String> patternList) {
+        Map<String, Long> select = new HashMap<>();
+        for(String pattern: patternList){
+            long generation = Fitness(pattern);
+            select.put(pattern, generation);
+        }
+
+        List<Map.Entry<String, Long>> list = new ArrayList<>(select.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
+            @Override
+            public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
+                return o2.getValue().compareTo(o1.getValue());   
+            }
+        });
+
+        return list.get(0).getKey();
     }
 }
