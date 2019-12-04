1eee2b2a3946fba17645bdf91b952f8ef8d86a5c
Kai Qian <32373116+kaiqiann@users.noreply.github.com>
2019-12-04 18:54:07 -0500

    genotype

---
+++ src/main/java/edu/neu/coe/info6205/life/base/Genotype.java
@@ -0,0 +1,32 @@
+package edu.neu.coe.info6205.life.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Genotype {
+	private List<Chromosome> geno;
+	
+	public Genotype() {
+		geno = new ArrayList<>();
+	}
+	
+	public void addChromosome(Chromosome chromosome) {
+		geno.add(chromosome);
+	}
+	
+	public List<Chromosome> getList(){
+		return geno;
+	}
+
+
+	public void setGeno(List<Chromosome> geno) {
+		this.geno = geno;
+	}
+
+	public List<Integer> intList(List<Chromosome> geno){
+		List<Integer> ls = new ArrayList<Integer>();
+		geno.forEach(g -> ls.add(g.getChromosome()));
+		
+		return ls;
+	}
+}
