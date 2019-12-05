66083097538dd36cc18fa93baa355df0221fd753
li-jiapeng <53372115+li-jiapeng@users.noreply.github.com>
2019-12-04 19:46:58 -0500

    Add files via upload

--- src/main/java/edu/neu/coe/info6205/life/base/Profile.java
+++ src/main/java/edu/neu/coe/info6205/life/base/Profile.java
@@ -3,4 +3,5 @@ package edu.neu.coe.info6205.life.base;
 public class Profile {
 	static final int MAX_GENERATION = 10000;
 	static final int CYCLECHECK_NUM = 3;
+	static final int GA_POPULATION = 1000;
 }
--- src/main/java/edu/neu/coe/info6205/life/base/geneticAlgorithm.java
+++ src/main/java/edu/neu/coe/info6205/life/base/geneticAlgorithm.java
@@ -1,17 +1,41 @@
 package edu.neu.coe.info6205.life.base;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class geneticAlgorithm {
+import com.google.common.collect.Lists;
 
+public class geneticAlgorithm {
+	
+	public List<String> initialPopulation(){
+		
+		List<String> population = new ArrayList<>();
+		for(int i = 0; i < Profile.GA_POPULATION;i++) {
+			InitialPattern ip = new InitialPattern();
+			population.add(ip.getPattern());
+		}
+		return population;
+	}
+	
 	public String run(List<String> population) {
 
 		for (int i = 0; i < Profile.MAX_GENERATION; i++) {
+			
 			Selector.Select(population);
+			List<String> current = new ArrayList<>();
+			for(String s:population) {
+				current.add(s);
+			}
+			
 			// population ==> chromosome
-			// Mutator.Mutate(gene);
-			//gene ==> poplulation;
+			for(String s:population) {
+				
+				Mutator.Mutate(gene);
+				
+				current.add(e);
+			}
+			population = current;
 		}
-		return "";//getBest(population);
+		return Selector.getBest(population);
 	}	
 }
