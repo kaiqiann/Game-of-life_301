ad68fe3b41b55065321cdd2ec1b11d7af973a63b
Kai Qian <32373116+kaiqiann@users.noreply.github.com>
2019-12-06 15:44:20 -0500

    initialpattern

--- src/main/java/edu/neu/coe/info6205/life/base/InitialPattern.java
+++ src/main/java/edu/neu/coe/info6205/life/base/InitialPattern.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.life.base;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -10,33 +11,51 @@ public class InitialPattern {
 //	private final int population = 1000;
 	private String pattern = "";
 	private List<String> plist = new ArrayList<>();
+	private long seed;
 	
 	public String getPattern() {
 		return pattern;
 	}
 
-	public InitialPattern() {
+	public InitialPattern(long seed) {
 		//add GA pattern here
-		initialize();
+		this.seed = seed;
+//		setSeed();
+		initialize(seed);
 	}
 	/**
 	 * Initialize function basically generates a string of points
 	 * 9 blocks as a unit to generate multiple points and append into string builder
 	 * uses set to eliminate duplicate points
 	 */
-	public void initialize() {
+	public void initialize(long seed) {
 		StringBuilder sb = new StringBuilder();
-//		int counter = 0;
 		Set<String> hs = new HashSet<String>();
-		for(int  x = 1; x <=20; x=x+3) {
-			for(int y = 1; y <=20; y=y+3) {
-				String temp = String.valueOf(x)+" "+String.valueOf(y);
-				sb.append(temp+ ", ");
+		Random rd = new Random(seed);
+//		int i =0;
+		/*while(i < Profile.initialcount) {
+			String temp = String.valueOf(rd.nextInt()*Profile.initialcount)+" "+String.valueOf(rd.nextInt()*Profile.initialcount);
+			if(!hs.contains(temp)) {
 				hs.add(temp);
-				int random = (int) (Math.random()*6);
+				sb.append(temp+ ", ");
+			}
+			i++;
+		}*/
+		
+		for(int  x = 0; x <=20; x=(int) (x+rd.nextDouble()*5)) {
+			for(int y = 0; y <=30; y=(int) (y+rd.nextDouble()*5)) {
+				String temp = String.valueOf(x)+" "+String.valueOf(y);
+				if(!hs.contains(temp)) {
+					hs.add(temp);
+					sb.append(temp+ ", ");
+				}
+//				rd = new Random(Profile.RANDOM_SEED);
+				int random = (int) (rd.nextDouble()*7);
 				for(int i =0; i<random;i++) {
-					int xaix = (int) (Math.random()*3)+x;
-					int yaix = (int) (Math.random()*3)+y;
+//					rd = new Random(Profile.RANDOM_SEED+10000);
+					int xaix = (int) (rd.nextDouble()*3)+x;
+//					rd = new Random(Profile.RANDOM_SEED+2000);
+					int yaix = (int) (rd.nextDouble()*3)+y;
 					temp = String.valueOf(xaix)+" "+String.valueOf(yaix);
 					if(!hs.contains(temp)) {
 						hs.add(temp);
@@ -47,19 +66,31 @@ public class InitialPattern {
 		}
 		pattern = sb.toString();
 		plist.add(pattern);
-//		System.out.println(counter);
 	}
 	
-	
+	public long getSeed() {
+		return seed;
+	}
+
+	public void setSeed() {
+		this.seed = System.currentTimeMillis();
+	}
+
 	public List<String> getPlist() {
 		return plist;
 	}
-
-	/*public static void main(String args[]) {
-		InitialPattern ip = new InitialPattern();
+/*
+	public static void main(String args[]) {
+		InitialPattern ip = new InitialPattern(Profile.RANDOM_SEED);
 		System.out.println(ip.getPattern());
-	}*/
-	
-	
+		Random rd = new Random(ip.getSeed());
+		System.out.println(rd.nextInt());
+		Random rd1 = new Random(ip.getSeed()+10000);
+		System.out.println(rd1.nextInt());
+		Random rd2 = new Random(ip.getSeed()+2000);
+		System.out.println(rd2.nextInt());
+	}
+
+*/
 
 }
