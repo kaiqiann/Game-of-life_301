e5e38321390707d1c276e6168d1b1a70218882d0
Kai Qian <32373116+kaiqiann@users.noreply.github.com>
2019-12-06 15:44:56 -0500

    Update Profile.java

--- src/main/java/edu/neu/coe/info6205/life/base/Profile.java
+++ src/main/java/edu/neu/coe/info6205/life/base/Profile.java
@@ -3,5 +3,8 @@ package edu.neu.coe.info6205.life.base;
 public class Profile {
 	static final int MAX_GENERATION = 5;
 	static final int CYCLECHECK_NUM = 10;
-	static final int GA_POPULATION = 2;
+	static final int GA_POPULATION = 3;
+	static final int RANDOM_SEED = 15755951;
+	static final int initialcount = 150;
+	
 }
