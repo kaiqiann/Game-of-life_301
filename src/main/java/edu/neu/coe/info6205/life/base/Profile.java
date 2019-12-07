be3d062c1a92455487f77ce91678116be00156f0
li-jiapeng <53372115+li-jiapeng@users.noreply.github.com>
2019-12-06 20:27:52 -0500

    Update Profile.java

--- src/main/java/edu/neu/coe/info6205/life/base/Profile.java
+++ src/main/java/edu/neu/coe/info6205/life/base/Profile.java
@@ -5,10 +5,22 @@ public class Profile {
 	static final int GAME_MAX_GENERATION = 2000;
 	
 	//Genetic Algorithm
-	static final int MAX_GENERATION = 3;
-	static final int GA_POPULATION = 4;
-	static final double SURVIVE_RATE = 0.25;
+	static final int MAX_GENERATION = 15;
+	
+	static final int GA_POPULATION = 20;
+	
 	static final int CYCLECHECK_NUM = 10;
-	static final int RANDOM_SEED = 15755951;
+	
+	static long RANDOM_SEED = 15755951;
+	
+	//Select
+	static final double SURVIVE_RATE = 0.25;
+	
+	//Mutate
+	static final double ADD_PROBABILITY = 0.15;
+	
+	static final double DELETE_PROBABILITY = 0.15;
+	
+	static final double MUTATE_PROBABILITY = 0.2;
 	
 }
